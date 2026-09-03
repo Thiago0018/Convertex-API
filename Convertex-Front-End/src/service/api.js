6b11fa17504ff516c380cbc4a975c4f6611e32e6
thiago0018 <thigos0018@gmail.com>
2026-09-03 18:22:28 -0300

    teste: hardcode url do render para eliminacao de cache

--- Convertex-Front-End/src/service/api.js
+++ Convertex-Front-End/src/service/api.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 // Força a decisão em tempo de execução no navegador
-const isLocalhost = typeof window !== 'undefined' &&
+/*const isLocalhost = typeof window !== 'undefined' &&
     (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1');
 
 const API_URL = isLocalhost
     ? 'http://localhost:5187/api'
     : 'https://convertex-api.onrender.com/api';
 
-console.log('[CONVERTEX] Conectando API em:', API_URL);
+console.log('[CONVERTEX] Conectando API em:', API_URL);*/
 
 export const api = axios.create({
-    baseURL: API_URL
+    baseURL: 'https://convertex-api.onrender.com/api'//API_URL
 });
